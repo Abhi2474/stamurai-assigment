f3ecbecf73189e366ffcb2fef67fbd12198dc55f
Aman <amangautam2474@gmail.com>
2023-07-12 12:57:56 +0530

    Update URL

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/Home";
 import ITask from "@/type";
 
 const fetchTasks = async () => {
-  const res = await fetch("http://localhost:3000/api");
+  const res = await fetch("/api");
   const data = await res.json();
   return data;
 };
